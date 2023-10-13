352d1d6a6a5777f04098cf72a6ed94acb1d38781
Antonius Silvianus <antoniussilvianus@gmail.com>
2023-10-13 17:08:21 +0700

    reformatting

--- public/js/script.js
+++ public/js/script.js
@@ -11,11 +11,11 @@ hamburger.addEventListener('click', () => {
 
 // Navbar Fixed
 
-window.onscroll = function() {
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav) {
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
     } else {
         header.classList.remove('navbar-fixed');
